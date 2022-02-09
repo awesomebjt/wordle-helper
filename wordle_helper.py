0aaf30d1a19243a215dbdf1b11043752431f62da
Benjamin J. Thompson <bjt@rabidquill.com>
2022-02-09 18:50:33 -0500

    Now stores all guessed letters in a string and uses it to offer suggestions using letters you have not tried yet.

--- wordle_helper.py
+++ wordle_helper.py
@@ -23,17 +23,31 @@ def rank(words):
 
 
 def word_matches(pattern, word):
+    # Returns true if the word matches the regex pattern
     return bool(re.match(pattern, word))
 
 
 def has_found_letters(letters_found, word):
+    # Returns true if all the letters can be found in the word
     return len([l for l in letters_found if l in word]) == len(letters_found)
+
+def excludes_all_letters(letters, word):
+    # Returns True if none of the letters can be found in the word
+    return len([l for l in letters if l in word]) == 0
+
+def valid_guess(guess_input):
+    return len([l for l in guess_input if l in alphabet]) == 5
+
+def valid_result(result_input):
+    return len([l for l in result_input if l in "xYG"]) == 5
+
 
 
 if __name__ == "__main__":
     wordlist = open(sys.argv[1], 'r').read().split("\n")
     excluded_letters = []
     found_letters = []
+    guessed_letters = ""
     solution = list(".....")
     result = "xxxxx"
     while result != "GGGGG":
@@ -46,14 +60,29 @@ if __name__ == "__main__":
         print("Discovered Letters:\t{}\n".format(", ".join([f[0] for f in found_letters])))
         print("Excluded Letters:\t{}\n".format(", ".join(excluded_letters)))
 
+        # Words that fit what is known about the word so far
         top_matching_words = rank([w for w in wordlist if
                                    word_matches(solution_regex, w)
                                    and has_found_letters([f[0] for f in found_letters], w)])[:10]
-        print("Suggested Guesses:")
+
+        # Words that will eliminate as many new letters as possible, even if it's wrong
+        intel_words = rank([w for w in wordlist if excludes_all_letters(guessed_letters, w)])[:5]
+        print("{} Suggested Guesses:".format(len(top_matching_words)))
         for tmw in top_matching_words:
             print(tmw)
-        guess = input("Input your Guess: ")
-        result = input("What was the result?\n(x for letter not found,\nG for green\nY for yellow): ")
+
+        print("""
+{} Words Excluding All Letters
+You Have Already Tried:""".format(len(intel_words)))
+        for iw in intel_words:
+            print(iw)
+        guess = ""
+        while not valid_guess(guess):
+            guess = input("Input your Guess (5 lowercase letters): ")
+        guessed_letters += guess
+        result = ""
+        while not valid_result(result):
+            result = input("What was the result?\n(x for letter not found,\nG for green\nY for yellow): ")
         for i in range(5):
             g = guess[i]
             r = result[i]
